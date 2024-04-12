dd2d37c9316cf4a8b1f8cf2f46ca2c4d485a3afa
Ebenolt <me@ebenolt.com>
2024-04-12 14:23:44 +0200

    1st commit (Readme, setup and basic conf)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# s3-backuper
+# S3 Backuper
+Simple file backup to S3
+
+#### Setup
+- Create your favorite S3 bucket
+- Rename `setup/config_template.json` to `setup/config.json`
+- Report any informations into `setup/config.json`
+- Run `setup.sh`
---
+++ setup/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear
+echo "Install required packages"
+sudo apt install awscli python3 python3-pip jq -y -q
+python3 -m pip install -r requirements.txt --break-system-packages
+
+echo "Configuring awscli (s3)"
+printf "
+[default]
+aws_access_key_id = $(cat config.json | jq -r .config.s3.access_key)
+aws_secret_access_key = $(cat config.json | jq -r .config.s3.secret_key)
+" > ~/.aws/credentials
+printf "
+[plugins]
+endpoint = awscli_plugin_endpoint
+
+[profile default]
+region = $(cat config.json | jq -r .config.s3.region | tr '[:upper:]' '[:lower:]')
+s3 =
+  endpoint_url = $(cat config.json | jq -r .config.s3.endpoint)
+  signature_version = s3v4
+s3api =
+  endpoint_url = $(cat config.json | jq -r .config.s3.endpoint)
+" > ~/.aws/config
+
+echo "Done !"
