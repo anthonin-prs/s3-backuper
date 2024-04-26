2199ee4f8620e109e640d6e9c430b96478970a06
Ebenolt <me@ebenolt.com>
2024-04-26 09:37:58 +0200

    Append to log file

--- backup.sh
+++ backup.sh
@@ -23,17 +23,17 @@ do
 
 
         if [[ $retention_limit -ge $file_date ]]; then
-            echo "  - Deleting s3://$dest_bucket/$s3_file" | tee $log_file_name
+            echo "  - Deleting s3://$dest_bucket/$s3_file" | tee -a $log_file_name
             aws s3 rm s3://$dest_bucket/$s3_file --quiet
         fi
 
     done
 
-    echo "Running '$name' backup: $source_folder => s3://$dest_bucket" | tee $log_file_name
+    echo "Running '$name' backup: $source_folder => s3://$dest_bucket" | tee -a $log_file_name
 
-    echo "  - Compressing $source_folder to $archive_name" | tee $log_file_name
+    echo "  - Compressing $source_folder to $archive_name" | tee -a $log_file_name
     tar -czf $archive_name $source_folder
-    echo "  - Uploading $archive_name to s3://$dest_bucket" | tee $log_file_name
+    echo "  - Uploading $archive_name to s3://$dest_bucket" | tee -a $log_file_name
     aws s3 cp $archive_name s3://$dest_bucket --quiet
     aws s3 cp $log_file_name s3://$dest_bucket --quiet
     
